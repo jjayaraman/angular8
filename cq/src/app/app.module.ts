8fbdf77e421e5baf716a3e3874a1a22c3451325f
jjayaraman <india.jai@gmail.com>
2019-06-06 21:37:53 +0100

    prime ng added

--- cq/src/app/app.module.ts
+++ cq/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {InputTextModule} from 'primeng/inputtext';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppCompatibilityRulesComponent } from './admin/app-compatibility-rules/app-compatibility-rules.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { AppCompatibilityRulesComponent } from './admin/app-compatibility-rules/
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
